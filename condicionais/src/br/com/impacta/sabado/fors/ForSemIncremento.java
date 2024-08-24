358c4d66212395bd2bfc229c99e799036d20f390
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-24 09:52:31 -0300

    For sem incremento...

---
+++ condicionais/src/br/com/impacta/sabado/fors/ForSemIncremento.java
@@ -0,0 +1,13 @@
+package br.com.impacta.sabado.fors;
+
+public class ForSemIncremento {
+
+	public static void main(String[] args) {
+		
+		for(int x = 10, y = 1; x != 1; System.out.println("Aqui não tem incremento")){
+			x = x - y;
+		}
+		 
+	}
+
+}
