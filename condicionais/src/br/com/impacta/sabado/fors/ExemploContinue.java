3ae51e3596f328badd628a92ee3da5d45cbf280e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-24 10:27:01 -0300

    Exemplo continue

---
+++ condicionais/src/br/com/impacta/sabado/fors/ExemploContinue.java
@@ -0,0 +1,20 @@
+package br.com.impacta.sabado.fors;
+
+public class ExemploContinue {
+
+	public static void main(String[] args) {
+		int num = 0;
+		while(num<10){
+			
+			++num;
+			
+			if(num==5){
+				continue;
+			}
+			
+			System.out.println("Valor de num = " + num);
+		}
+
+	}
+
+}
